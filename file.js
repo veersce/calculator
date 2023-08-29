a357c1757747695f2fd221c5f3e644f8ac4a2149
Veris <141730322+ImVeris@users.noreply.github.com>
2023-08-29 16:16:36 +0200

    Finished calculator. It has some flaws.

--- file.js
+++ file.js
@@ -1,89 +1,112 @@
-let number1 = 0;
-let number2 = 0;
-let operator = "";
+let firstOperand = 0;
+let secondOperand = 0;
+let firstOperator = "";
+let displayValue = "";
 
-let displayValue = 0;
-let display = document.querySelector('.display');
-let numberButtons = document.querySelectorAll('.number');
+const display = document.querySelector('.display');
 
-numberButtons.forEach(button =>
-    button.addEventListener('click', (e) =>
-    {
-        display.textContent += e.target.textContent;
-        // displayValue = Number(display.textContent);
-    })
-)
+window.addEventListener('keydown', (e) => {
+    e.preventDefault();
+    let button = document.querySelector(`button[data-key=${e.code}]`);
+    button.click();
+});
 
-let operators = document.querySelectorAll('.operator');
-operators.forEach(operatorKey =>
-    operatorKey.addEventListener('click', (e) =>
-    {
-        number1 = Number(display.textContent);
-        operator = operatorKey.textContent;
-        display.textContent = "0";
-    })
-)
-
-let equalsKey = document.querySelector('.equals');
-equalsKey.addEventListener('click', () =>
-{
-    number2 = Number(display.textContent);
-    display.textContent = operate(number1, number2, operator);
+const periodButton = document.querySelector('.period');
+periodButton.addEventListener('click', () => {
+    if (!displayValue.includes('.')) {
+        displayValue += periodButton.textContent;
+        updateDisplay();
+    }
 })
 
-let clearKey = document.querySelector('.clear');
-clearKey.addEventListener('click', () => 
-{
-    display.textContent = "0";
-    number1 = 0;
-    number2 = 0;
-    operator = "";
+const backspaceButton = document.querySelector('.backspace');
+backspaceButton.addEventListener('click', () => {
+    displayValue = displayValue.slice(0, -1);
+    updateDisplay();
 })
 
-function operate(num1, num2, operator)
-{
+const signButton = document.querySelector('.sign');
+signButton.addEventListener('click', () => {
+    displayValue = String(displayValue * -1);
+    updateDisplay();
+})
+
+function toFixedIfNecessary(value, dp) {
+    return +parseFloat(value).toFixed(dp);
+}
+
+function updateDisplay() {
+    if (String(displayValue.length) <= 15) {
+        display.textContent = String(displayValue);
+    }
+}
+
+const operators = document.querySelectorAll('.operator');
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        firstOperand = Number(displayValue);
+        firstOperator = operator.textContent;
+        displayValue = "";
+        if (firstOperand) {
+            secondOperand = Number(displayValue);
+        }
+    })
+})
+
+const equalsButton = document.querySelector('.equals');
+equalsButton.addEventListener('click', () => {
+    if (!firstOperator)
+        return;
+    secondOperand = Number(displayValue);
+    displayValue = calculate(firstOperand, secondOperand, firstOperator);
+    updateDisplay();
+})
+
+const numberButtons = document.querySelectorAll('.number');
+numberButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        displayValue += btn.textContent;
+        updateDisplay();
+    })
+})
+
+function clear() {
+    firstOperand = 0;
+    secondOperand = 0;
+    firstOperator = "";
+    displayValue = "0";
+}
+
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', () => {
+    clear();
+    updateDisplay();
+})
+
+function calculate(a, b, op) {
     let result = 0;
-    switch (operator)
-    {
+    switch (op) {
         case '+':
-            result = add(num1, num2);
+            result = a + b;
             break;
-
         case '-':
-            result = subtract(num1, num2);
+            result = a - b;
             break;
-
         case '*':
-            result = multiply(num1, num2);
+            result = a * b;
             break;
-
         case '/':
-            result = divide(num1, num2);
+            if (b === 0 || b === '0') {
+                alert("Wait, that's illegal.");
+                clear();
+                return "0";
+            }
+            else {
+                result = a / b;
+            }
             break;
-
         default:
-            console.log("Wrong operator")
             break;
     }
-    return result;
-}
-
-function add(a, b)
-{
-    return a + b;
-}
-
-function subtract(a, b)
-{
-    return a - b;
-}
-
-function multiply(a, b)
-{
-    return a * b;
-}
-
-function divide(a, b)
-{
-    return a / b;
+    return String(toFixedIfNecessary(result, 3));
 }
